236fd07d98d4cdc8730f209b558f38bf1d557c19
dzmitry <dzmitry.vashkevich.a@gmail.com>
2022-11-16 18:08:42 +0300

    add trace result serializer

---
+++ TraceResultSerializer/JsonSerializer.cs
@@ -0,0 +1,15 @@
+﻿using Tracer;
+using System.Text.Json;
+
+namespace TraceResultSerializer
+{
+    public class JsonSerializer : ITraceResultSerializer<TraceResult>
+    {
+        public string Serialize(TraceResult traceResult)
+        {
+            return System.Text.Json.JsonSerializer.Serialize(traceResult,
+               typeof(TraceResult),
+               new JsonSerializerOptions { WriteIndented = true });
+        }
+    }
+}
---
+++ TraceResultSerializer/XmlSerializer.cs
@@ -0,0 +1,34 @@
+﻿using Tracer;
+using System.Xml.Linq;
+
+namespace TraceResultSerializer
+{
+    public class XmlSerializer : ITraceResultSerializer<TraceResult>
+    {
+        public string Serialize(TraceResult traceResult)
+        {
+            System.Xml.Serialization.XmlSerializer xmlSerializer =
+                new System.Xml.Serialization.XmlSerializer(typeof(TraceResult));
+
+            using (var stringWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(stringWriter, traceResult);
+                return FormatXml(stringWriter.ToString());
+            }
+
+        }
+        private static string FormatXml(string xml)
+        {
+            try
+            {
+                XDocument doc = XDocument.Parse(xml);
+                return doc.ToString();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("uncoorect xml");
+                return xml;
+            }
+        }
+    }
+}
