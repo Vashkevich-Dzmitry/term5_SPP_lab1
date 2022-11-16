eab48297448546609d9ef6805ce37876af80420f
dzmitry <dzmitry.vashkevich.a@gmail.com>
2022-11-16 17:30:53 +0300

    add serialization writers

---
+++ SPP_lab1/SerializationWriters/SerializationConsoleWriter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPP_lab1
+{
+    internal class SerializationConsoleWriter : ISerializationWriter
+    {
+        public void Write(string value)
+        {
+            Console.Write(value);
+        }
+    }
+}
---
+++ SPP_lab1/SerializationWriters/SerializationFileWriter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPP_lab1
+{
+    internal class SerializationFileWriter : ISerializationWriter
+    {
+        public string Path { get; set; }
+        public SerializationFileWriter(string path)
+        {
+            Path = path;
+        }
+        public void Write(string value)
+        {
+            using (StreamWriter writer = new StreamWriter(Path))
+            {
+                writer.Write(value);
+            }
+        }
+    }
+}
